cc3123ce804aecd8f8609a224afff575037420b9
Anubhav Purohit <purohitanubhav@yahoo.com>
2020-09-23 10:29:09 +0530

    Improved Styling and added Background image

--- script.js
+++ script.js
@@ -2,9 +2,21 @@ var submit= document.getElementById("s");
 
 submit.addEventListener("click" , copy);
 
+
+
 function copy(){
   let user=document.getElementById("t").value;
 
   document.getElementById('p').value=user;
   console.log(document.getElementById('p').value);
+}
+
+var reset=document.getElementById("r");
+
+reset.addEventListener("click", reset);
+function reset(){
+
+  document.getElementById('t').value='';
+  document.getElementById('p').value='';
+
 }
